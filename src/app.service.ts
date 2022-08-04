500094b1c879361f96aa70dbf0bd7c281375c34f
윤민아 <green@guarantable.com>
2022-08-04 22:43:23 +0900

    asdfSA

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Hello World! ovni! ${process.env.MINA}::`;
+    return `Hello World! mins! ${process.env.MINA}::`;
   }
 }
